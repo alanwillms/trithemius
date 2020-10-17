11193d41579720c40f0113d1aec6db719c0236ec
Alan Willms <dyulax@gmail.com>
2020-10-17 18:09:20 -0300

    Textarea autofit to contents

--- src/components/CatTranslatedParagraph.vue
+++ src/components/CatTranslatedParagraph.vue
@@ -2,29 +2,44 @@
   <div>
     <div v-if="isEditable">
       <textarea
+        id="text-being-edited"
         :class="{
           'p-4': true,
           border: true,
           'font-mono': true,
           'border-transparent': !isEditable,
           'border-red-700': isEditable,
-          'w-full': true
+          'w-full': true,
+          'resize-none': true,
         }"
         :value="textBeingEdited"
-        @input="setTextBeingEdited($event.target.value)"
-        autofocus>
+        @input="inputHandler($event)"
+        autofocus
+      >
       </textarea>
 
-      <div class="sans-serif mt-2 flex w-full justify-end">
-        <button @click="$emit('repeat')" type="button" class="mx-2 rounded bg-orange-600 text-white py-2 px-4">
+      <div class="sans-serif p-4 flex w-full justify-end">
+        <button
+          @click="$emit('repeat')"
+          type="button"
+          class="rounded bg-orange-600 text-white py-2 px-4"
+        >
           Repeat
         </button>
 
-        <button @click="$emit('cancel')" type="button" class="mx-2 rounded bg-red-600 text-white py-2 px-4">
+        <button
+          @click="$emit('cancel')"
+          type="button"
+          class="ml-4 rounded bg-red-600 text-white py-2 px-4"
+        >
           Cancel
         </button>
 
-        <button @click="$emit('save')" type="button" class="mx-2 rounded bg-blue-600 text-white py-2 px-4">
+        <button
+          @click="$emit('save')"
+          type="button"
+          class="ml-4 rounded bg-blue-600 text-white py-2 px-4"
+        >
           Save
         </button>
       </div>
@@ -39,14 +54,20 @@
         'font-serif': true,
         'border-transparent': true,
       }"
-      @click="$emit('select')">
-      <div v-for="(line, lineKey) in paragraph.translation.split('\n')" v-bind:key="lineKey">{{ line }}</div>
+      @click="$emit('select')"
+    >
+      <div
+        v-for="(line, lineKey) in paragraph.translation.split('\n')"
+        v-bind:key="lineKey"
+      >
+        {{ line }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, nextTick, watch } from 'vue'
 import { useStore } from 'vuex'
 export default {
   props: {
@@ -56,20 +77,44 @@ export default {
     },
     isEditable: {
       type: Boolean,
-      default: false
+      default: false,
     },
     modelValue: String,
   },
-  setup () {
+  setup(props) {
     const store = useStore()
     const textBeingEdited = computed(() => store.state.textBeingEdited)
-    const setTextBeingEdited = (value) => {
+    const setTextBeingEdited = value => {
       store.dispatch('setTextBeingEdited', value)
     }
-    return {
-      textBeingEdited,
-      setTextBeingEdited
+    const autoGrow = element => {
+      try {
+        element.style.height = '5px'
+        element.style.height = element.scrollHeight + 10 + 'px'
+      } catch (error) {
+        console.log(error)
+      }
     }
-  }
+    watch(
+      () => props.isEditable,
+      value => {
+        if (value) {
+          nextTick(() => {
+            const element = document.getElementById('text-being-edited')
+            autoGrow(element)
+            element.focus()
+          })
+        }
+      },
+    )
+    const inputHandler = $event => {
+      autoGrow($event.target)
+      setTextBeingEdited($event.target.value)
+    }
+    return {
+      inputHandler,
+      textBeingEdited,
+    }
+  },
 }
 </script>
